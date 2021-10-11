8b84a3f923f8d237202a719457412fecef984be4
a2ma <a2ma@outlook.com>
2021-10-11 00:30:12 -0400

    PPP with CPP by Stroustrup, Chapter 8 exercises

---
+++ ppp-stroustrup-ch8/ppp-stroustrup-ch8/ppp-stroustrup-ch8.cpp
@@ -0,0 +1,20 @@
+//#include "../../std_lib_facilities.h"
+#include "my.h"
+#include <iostream>
+
+int main()
+{
+	/*cout << "Hello, world!\n";
+	int a{ 9 };
+	cout << "variable a = " << a << ".\n";
+	const int b{ 10 };
+	cout << "const b = " << b << ".\n";
+
+	vector<int> myintvector{ 1, 2, 3, 4, 5, 6 };*/
+	foo = 7;
+	print_foo();
+	print(99);
+	char cc; std::cin >> cc;
+}
+
+
