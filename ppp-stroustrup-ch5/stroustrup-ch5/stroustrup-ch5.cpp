3c335c8976a709734b1f960b877a5764621372ba
a2ma <a2ma@outlook.com>
2021-10-15 21:45:49 -0400

    exercises from chapter 15 try this

--- ppp-stroustrup-ch5/stroustrup-ch5/stroustrup-ch5.cpp
+++ ppp-stroustrup-ch5/stroustrup-ch5/stroustrup-ch5.cpp
@@ -1,8 +1,65 @@
 #include "../../std_lib_facilities.h"
 
+// ask user for a yes-or-no answer;
+// return 'b' to indicate a bad answer (i.e., not yes or no)
+char ask_user(string question)
+{
+	cout << question << "? (yes or no)\n";
+	string answer = " ";
+	cin >> answer;
+	if (answer == "y" || answer == "yes") return 'y';
+	if (answer == "n" || answer == "no") return 'n';
+	return 'b';    // 'b' for "bad answer"
+}
+
+//int area(int length, int width)
+//{
+//	return length * width;
+//}
+
+int area(int length, int width);    // calculate area of a rectangle
+
+int framed_area(int x, int y)       // calculate area within frame
+{
+	const int frame_width = 2;
+	cout << x - frame_width << "\n";
+	cout << y - frame_width << "\n";
+	if (x - frame_width <= 0 || y - frame_width <= 0)
+		error("non-positive argument for area() called by framed_area()");
+	return area(x - frame_width, y - frame_width);
+}
+
+int area(int length, int width)
+{
+	if (length <= 0 || width <= 0) return -1;
+	return length * width;
+}
+
+void f(int x, int y, int z)
+{
+	cout << "x=" << x << "; y=" << y << "; z=" << z << endl;
+
+	int area1 = area(x, y);
+	if (area1 <= 0) error("non-positive area");
+	int area2 = framed_area(1, z);
+	int area3 = framed_area(y, z);
+	double ratio = double(area1) / area3;
+	// ...
+
+	cout << "area1=" << area1 << endl
+		<< "area2=" << area2 << endl
+		<< "area3=" << area3 << endl;
+}
+
+//constexpr int frame_width = 2;
+//int framed_area(int x, int y)
+//{
+//	return area(x - frame_width, y - frame_width);
+//}
+
 int main()
 {
-	
+
 
 	//vector<double> v(10);
 	//vector<int> v2(10);
@@ -116,4 +173,54 @@ int main()
 	//	keep_window_open();
 	//	return 2;
 	//}
+
+// TRY THIS
+// pg. 138: skipped
+// pg. 139: skipped
+// pg. 146:
+
+	/*try
+	{
+		int x = -1;
+		int y = 2;
+		int z = 4;
+
+		if (x <= 0) error("non-positive x");
+		if (y <= 0) error("non-positive y");
+
+		int area1 = area(x, y);
+		int area2 = framed_area(1, z);
+		int area3 = framed_area(y, z);
+		double ratio = double(area1) / area3;
+	}
+	catch (exception& e) {
+		cerr << "error: " << e.what() << '\n';
+		return 1;
+	}
+	catch (...) {
+		cerr << "Oops: unknown exception! \n";
+		return 2;
+	}*/
+
+	try
+	{
+		f(3, 3, 3);
+		f(-1, 2, 3);
+		f(1, -2, 3);
+		f(-1, -2, 3);
+		f(1, 2, -3);
+		f(-1, 2, -3);
+		f(1, -2, -3);
+		f(-1, -2, -3);
+		char answer = ask_user("Have you read results?");
+	}
+	catch (exception& e) {
+		cerr << "error: " << e.what() << '\n';
+		return 1;
+	}
+	catch (...) {
+		cerr << "Oops: unknown exception!\n";
+		return 2;
+
+	}
 }
